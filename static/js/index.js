25edba1c376e068410cd7bef4894fc7c84dba341
mojoee <47809126+mojoee@users.noreply.github.com>
2024-02-20 11:20:57 +0800

    added edit delete buttons

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+// static/js/index.js
+
+import { DebugForm } from "./debug.js";
+import { People } from "./people.js";
+import { Notes } from "./notes.js";
+
+
+function main() {
+  new People();
+  new Notes();
+  if (document.querySelector(".debug-card")) {
+    const debug = new DebugForm();
+    debug.showResponse("");
+  }
+}
+
+main();
